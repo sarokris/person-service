971204672c2977aa145e3b14ca0a070c23961260
sarokris <saravananand.k18@gmail.com>
2022-10-19 13:40:56 +0530

    initial commit

---
+++ person-functions/src/main/java/org/example/PersonLambda.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * Lambda functions for Person service!
+ *
+ */
+public class PersonLambda
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
