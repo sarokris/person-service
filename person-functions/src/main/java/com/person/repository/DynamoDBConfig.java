1c29f4b217e26d2f13b9cc11629c8d5d634830d4
sarokris <saravananand.k18@gmail.com>
2022-10-20 19:47:57 +0530

    AccountID and Region config moved to environment variable

--- person-functions/src/main/java/com/person/repository/DynamoDBConfig.java
+++ person-functions/src/main/java/com/person/repository/DynamoDBConfig.java
@@ -1,8 +1,5 @@
 package com.person.repository;
 
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
--- person-infra/src/main/java/com/myorg/PersonInfraApp.java
+++ person-infra/src/main/java/com/myorg/PersonInfraApp.java
@@ -8,27 +8,17 @@ import java.util.Arrays;
 
 public class PersonInfraApp {
 
-    public static final String ACCOUNT_ID = "662065144489";
-    public static final String REGION_AP_SOUTH_1 = "ap-south-1";
     public static final String PERSON_INFRA_STACK_ID = "PersonInfraStack";
 
     public static void main(final String[] args) {
         App app = new App();
 
         new PersonInfraStack(app, PERSON_INFRA_STACK_ID, StackProps.builder()
-                /*
                 .env(Environment.builder()
                         .account(System.getenv("CDK_DEFAULT_ACCOUNT"))
                         .region(System.getenv("CDK_DEFAULT_REGION"))
                         .build())
-                */
 
-                // Uncomment the next block if you know exactly what Account and Region you
-                // want to deploy the stack to.
-                .env(Environment.builder()
-                        .account(ACCOUNT_ID)
-                        .region(REGION_AP_SOUTH_1)
-                        .build())
                 .build());
 
         app.synth();
