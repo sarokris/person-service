f770cf0eb2561e4721134c7ef36fbca13032bc32
sarokris <saravananand.k18@gmail.com>
2022-10-20 17:14:20 +0530

    Refactored the infra stack

--- person-infra/src/main/java/com/myorg/PersonInfraStack.java
+++ person-infra/src/main/java/com/myorg/PersonInfraStack.java
@@ -11,18 +11,34 @@ import software.amazon.awscdk.services.dynamodb.TableProps;
 import software.amazon.awscdk.services.lambda.Code;
 import software.amazon.awscdk.services.lambda.Function;
 import software.amazon.awscdk.services.lambda.FunctionProps;
+import software.amazon.awscdk.services.lambda.HttpMethod;
 import software.amazon.awscdk.services.lambda.Runtime;
 import software.constructs.Construct;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
+import static java.util.Collections.singletonList;
+
 public class PersonInfraStack extends Stack {
+
+    //DynamoDB properties
     public static final String DYNAMO_TABLE_NAME = "Persons";
     public static final String DYNAMO_PRIMARY_KEY = "id";
+
+    //Handler names
+    public static final String CREATE_FUN_HANDLER = "com.person.handler.PersonCreateHandler";
+    public static final String GET_FUN_HANDLER = "com.person.handler.PersonGetHandler";
+    public static final String FIND_ALL_FUN_HANDLER = "com.person.handler.PersonFindAllHandler";
+
+    //Lambda function names
+    public static final String FUNCTION_CREATE_PERSON = "CreatePerson";
+    public static final String FUNCTION_GET_PERSON = "GetPerson";
+    public static final String FUNCTION_FIND_ALL_PERSON = "FindAllPerson";
 
     public PersonInfraStack(final Construct scope, final String id) {
         this(scope, id, null);
@@ -30,6 +46,53 @@ public class PersonInfraStack extends Stack {
 
     public PersonInfraStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
+
+        Table dynamoTable = consturctDynamoTable();
+        constructLambdaFunctionsApis(dynamoTable);
+    }
+
+    private void constructLambdaFunctionsApis(Table dynamoTable) {
+
+        Map<String,String> lambdaEnvMap = new HashMap<>();
+        lambdaEnvMap.put("TABLE_NAME",dynamoTable.getTableName());
+        lambdaEnvMap.put("PRIMARY_KEY", DYNAMO_PRIMARY_KEY);
+
+        // Lambda functions
+        Function createPersonFun = new Function(this, FUNCTION_CREATE_PERSON
+                ,getLambdaFunProps(FUNCTION_CREATE_PERSON, CREATE_FUN_HANDLER,lambdaEnvMap));
+        Function getPersonFun = new Function(this, FUNCTION_GET_PERSON
+                ,getLambdaFunProps(FUNCTION_GET_PERSON, GET_FUN_HANDLER,lambdaEnvMap));
+        Function findAllPersonFun = new Function(this, FUNCTION_FIND_ALL_PERSON
+                ,getLambdaFunProps(FUNCTION_FIND_ALL_PERSON, FIND_ALL_FUN_HANDLER,lambdaEnvMap));
+
+        dynamoTable.grantReadWriteData(createPersonFun);
+        dynamoTable.grantReadWriteData(getPersonFun);
+        dynamoTable.grantReadWriteData(findAllPersonFun);
+
+        RestApiProps restApiProps = RestApiProps.builder()
+                .restApiName("Person Service")
+                .binaryMediaTypes(singletonList(ContentType.APPLICATION_JSON.getMimeType()))
+                .build();
+
+        RestApi restApi = new RestApi(this,"personApi",restApiProps);
+
+        //configuring the requestmapping
+        IResource personResource = restApi.getRoot().addResource("person");
+
+        //Lambda integration
+        Integration createIntegration = new LambdaIntegration(createPersonFun);
+        personResource.addMethod(HttpMethod.POST.name(),createIntegration);
+
+        //findById adding path param placeholder
+        IResource pathParamResource = personResource.addResource("{id}");
+        Integration getPersonIntgrtn = new LambdaIntegration(getPersonFun);
+        pathParamResource.addMethod(HttpMethod.GET.name(),getPersonIntgrtn);
+
+        Integration findAllPersonIntgrtn = new LambdaIntegration(findAllPersonFun);
+        personResource.addMethod(HttpMethod.GET.name(),findAllPersonIntgrtn);
+    }
+
+    private Table consturctDynamoTable() {
 
         Attribute partitionKey = Attribute.builder()
                 .name(DYNAMO_PRIMARY_KEY)
@@ -42,48 +105,7 @@ public class PersonInfraStack extends Stack {
                 .removalPolicy(RemovalPolicy.DESTROY)
                 .build();
 
-        Table dynamoTable = new Table(this, DYNAMO_TABLE_NAME,tableProps);
-
-        Map<String,String> lambdaEnvMap = new HashMap<>();
-        lambdaEnvMap.put("TABLE_NAME",dynamoTable.getTableName());
-        lambdaEnvMap.put("PRIMARY_KEY", DYNAMO_PRIMARY_KEY);
-
-        String createFunHandlerName = "com.person.handler.PersonCreateHandler";
-        String getFunHandlerName = "com.person.handler.PersonGetHandler";
-        String findAllFunHandlerName = "com.person.handler.PersonFindAllHandler";
-
-        Function createPersonFun = new Function(this,"CreatePerson"
-                ,getLambdaFunProps("CreatePerson",createFunHandlerName,lambdaEnvMap));
-        Function getPersonFun = new Function(this,"GetPerson"
-                ,getLambdaFunProps("GetPerson",getFunHandlerName,lambdaEnvMap));
-        Function findAllPersonFun = new Function(this,"FindAllPerson"
-                ,getLambdaFunProps("FindAllPerson",findAllFunHandlerName,lambdaEnvMap));
-
-        dynamoTable.grantReadWriteData(createPersonFun);
-        dynamoTable.grantReadWriteData(getPersonFun);
-        dynamoTable.grantReadWriteData(findAllPersonFun);
-
-        RestApiProps restApiProps = RestApiProps.builder()
-                .restApiName("Person Service")
-                .binaryMediaTypes(Arrays.asList(ContentType.APPLICATION_JSON.getMimeType()))
-                .build();
-        RestApi restApi = new RestApi(this,"personApi",restApiProps);
-
-        //configuring the requestmapping
-        IResource personResource = restApi.getRoot().addResource("person");
-
-        //Lambda integration
-        Integration createIntegration = new LambdaIntegration(createPersonFun);
-        personResource.addMethod("POST",createIntegration);
-
-        //findById adding path param placeholder
-        IResource pathParamResource = personResource.addResource("{id}");
-        Integration getPersonIntgrtn = new LambdaIntegration(getPersonFun);
-        pathParamResource.addMethod("GET",getPersonIntgrtn);
-
-        Integration findAllPersonIntgrtn = new LambdaIntegration(findAllPersonFun);
-        personResource.addMethod("GET",findAllPersonIntgrtn);
-
+       return new Table(this, DYNAMO_TABLE_NAME,tableProps);
 
     }
 
